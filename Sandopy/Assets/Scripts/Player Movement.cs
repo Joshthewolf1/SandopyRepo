16367bea4d10458196dbc21b4dd69336b930e4e9
Joshthewolf1 <joshua.rente57@gmail.com>
2023-10-12 10:06:21 -0700

    Moved Sandopy Project Into Repo

---
+++ Sandopy/Assets/Scripts/Player Movement.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Tilemaps;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour
+{
+    
+    [SerializeField] float runSpeed = 10f;
+    [SerializeField] float jumpSpeed = 5f;
+
+    Vector2 moveInput;
+    Rigidbody2D myRigidbody;
+    Animator myAnimator;
+
+    void Start()
+    {
+        GameObject.FindWithTag("Water").GetComponent<TilemapCollider2D>().enabled = false;
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        Run();
+        FlipSprite();
+        
+
+
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+    void OnJump(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            myRigidbody.velocity += new Vector2(0f, jumpSpeed);
+            myAnimator.SetBool("isSailing", false);
+            GameObject.FindWithTag("Water").GetComponent<TilemapCollider2D>().enabled = false;
+        }
+    }
+
+    void OnSail(InputValue value)
+    {
+
+        if (value.isPressed)
+       
+        {
+            myAnimator.SetBool("isSailing", true);
+            GameObject.FindWithTag("Water").GetComponent<TilemapCollider2D>().enabled = true;
+        }
+       
+    }
+
+
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, myRigidbody.velocity.y);
+        myRigidbody.velocity = playerVelocity;
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("isRunning", playerHasHorizontalSpeed);
+
+    }
+
+    private void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidbody.velocity.x), 1f);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Water"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+    
+    
+       
+    
+
+
+
+
+
+
+
+}
